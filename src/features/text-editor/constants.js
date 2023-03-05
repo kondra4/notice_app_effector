79ddc095a1dbad7cdbff21606e3ed92e8f8091be
kondra4 <kondra405@gmail.com>
2023-03-05 21:48:44 +0300

    feat(add-image): add function addImage

---
+++ src/features/text-editor/constants.js
@@ -0,0 +1,5 @@
+export const TEXT = {
+    addImage: 'Загрузить фото'
+}
+
+export const DEFAULT_IMAGE = 'https://sun9-78.userapi.com/s/v1/ig2/kx6abzNGmVJ0zYfaJCyWBm0HyhwWn1UF8HkgJi0C5cgHH01JXeQPUDFwzGNgMUog-9f65FV1dvjixZNL9CODJj1H.jpg?size=100x100&quality=96&crop=0,0,1200,1200&ava=1'
